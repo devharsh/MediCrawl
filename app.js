52b66846e5ec89259332147b9369ec8bb7fc34bf
Vaishnavi Gopalakrishnan <vaishnavimecit@gmail.com>
2020-04-29 11:50:15 -0400

    UI updated
    Run index.html

--- app.js
+++ app.js
@@ -2,36 +2,14 @@ const express = require("express");
 const app = express();
 const request = require('request');
 
-// var solrClient = require('solr-node');
-// var solr = require('solr-client');
-
-// var options = new solrClient({
-//     host: '127.0.0.1',
-//     port: '8983',
-//     path: '/solr/',
-//     core: 'nutch',
-//     protocol: 'http'
-// });
-
-// var client = solr.createClient();
-
-// client.add({id:12, title_t:'Hello'}, function(err, obj){
-//     if(err){
-//         console.log(err);
-//     }
-//     else{
-//         console.log('Solr response: ', obj);
-//     }
-// })
-
 
 app.get('/', (req, res) => {
-    //var search = req.body.params;
+    var search = req.body.params;
+    search = toLowerCase(str.replace(/\s+/g, ''));
+
     //var strQuery = options.query().q('cancer');
 
-    //request("https://www.fanlab.me/#people",
-    request("http://localhost:8983/solr/nutch/select?q=cancer&fl=url,title",
-    //client.search(strQuery, 
+    request("http://localhost:8983/solr/nutch/select?q=search&fl=url,title",
     function(error, response, body){
         console.error('Error: ', error);
         res.status(200).send(body);
--- node_modules/solr-node/index.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by godong on 2016. 3. 9..
- */
-
-module.exports = exports = require("./lib/client");
